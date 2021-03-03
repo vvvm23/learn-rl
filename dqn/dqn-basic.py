0f9e8b70c365f2764b044696d2a30c8b51ed1ec8
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2021-03-03 12:23:22 +0000

    render option and saving

--- dqn/dqn-basic.py
+++ dqn/dqn-basic.py
@@ -3,10 +3,12 @@ import gym
 from tqdm import tqdm
 
 import argparse
+import datetime
+from pathlib import Path
 
 from agent import DQNAgent 
 from memory.memory import ExperienceBuffer
-from models.dqn import DQN
+from models.dqn import DQN, DQNLinear
 from wrappers import make_env
 from helper import EpisilonAnnealer, VisdomLinePlotter
 
@@ -26,7 +28,10 @@ if __name__ == '__main__':
     obs_shape = env.observation_space.shape
     nb_actions = env.action_space.n
 
-    net = DQN(obs_shape, nb_actions)
+    if params.net_type == 'conv':
+        net = DQN(obs_shape, nb_actions)
+    elif params.net_type == 'linear':
+        net = DQNLinear(obs_shape, nb_actions)
     agent = DQNAgent(
         net=net, 
         nb_actions=nb_actions, 
@@ -37,6 +42,18 @@ if __name__ == '__main__':
 
     opt = torch.optim.Adam(net.parameters(), lr=params.learning_rate)
     eps_schedule = EpisilonAnnealer(params.epsilon_start, params.epsilon_end, params.epsilon_frames)
+
+    save_id = str(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+    path_str = lambda p: p.absolute().as_posix() # converts a pathlib Path to string
+
+    # Create the run directories
+    runs_dir = Path(f"runs")
+    root_dir = runs_dir / f'run-{save_id}'
+    chk_dir = root_dir / 'checkpoints'
+
+    runs_dir.mkdir(exist_ok=True) 
+    root_dir.mkdir(exist_ok=True)
+    chk_dir.mkdir(exist_ok=True)
 
     # TODO: Merge these two loops? maybe..
     # initial population of buffer
@@ -53,13 +70,15 @@ if __name__ == '__main__':
             next_obs = env.reset()
         obs = next_obs
 
-
     episode_reward = 0.0
     episode_count = 0
 
     pb = tqdm(range(params.max_steps))
     plotter = VisdomLinePlotter()
     for i in pb:
+        if args.render:
+            env.render()
+
         obs = obs.squeeze(0)
         idx = memory.store_obs(obs)
         state = memory.get_stacked_obs(idx)
@@ -76,6 +95,9 @@ if __name__ == '__main__':
             pb.set_description(f"episode: {episode_count}, reward: {episode_reward}, eps: {eps_schedule.get(i)*100:.2f}%")
             plotter.plot('episode reward', 'foo', "Episode Return", episode_count, episode_reward)
             episode_reward = 0
+            if episode_count > 0 and episode_count % params.save_frequency == 0:
+                agent.save(chk_dir/ f"checkpoint-episode-{episode_count}.pt")
+
         obs = next_obs
 
         if i % params.target_sync == 0:
