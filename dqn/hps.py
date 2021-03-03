5e172a5046331524e2d3b06a95906e1ca0e32c5c
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2021-03-03 00:05:24 +0000

    adds hyperparamets using simplenamespace

---
+++ dqn/hps.py
@@ -0,0 +1,18 @@
+from types import SimpleNamespace
+
+HPS_BASIC = {
+    'pong': SimpleNamespace(**{
+        'env_name':             "PongNoFrameskip-v4",
+        'stop_reward':          18.0,
+        'max_steps':            2_000_000,
+        'memory_capacity':      100_000,
+        'memory_initial':       10_000,
+        'target_sync':          1000,
+        'epsilon_start':        1.0,
+        'epsilon_end':          0.01,
+        'epsilon_frames':       100_000,
+        'learning_rate':        1e-4,
+        'gamma':                0.99,
+        'batch_size':           32,
+    })
+}
