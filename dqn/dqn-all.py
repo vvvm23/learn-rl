b507bc876eeb0daef78c653de4cc06591c6ff0e1
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2021-04-10 10:48:34 +0100

    Adds rainbow script not true rainbow, as missing categorical dqn

---
+++ dqn/dqn-all.py
@@ -0,0 +1,141 @@
+import torch
+import gym
+import numpy as np
+from tqdm import tqdm
+
+import argparse
+import datetime
+from pathlib import Path
+from collections import deque
+from time import sleep
+
+from agent import DQNAgent 
+from memory.memory import ExperienceBuffer
+from models.rainbow import DQN
+from wrappers import make_env
+from helper import EpisilonAnnealer, VisdomLinePlotter
+
+from hps import HPS_BASIC as HPS
+
+@torch.no_grad()
+def evaluate(agent, env, render=False):
+    while True:
+        obs = env.reset()
+        state = torch.zeros(params.frame_stack, *obs_shape)
+        done = False
+        total_return = 0.
+        while not done:
+            if render:
+                env.render()
+                sleep(1 / 60.)
+            state = torch.cat([state[1:], torch.from_numpy(obs).unsqueeze(0)], dim=0)
+            action = agent.act_epsilon_greedy(state, 0.02)
+            obs, reward, done, _ = env.step(action)
+            total_return += reward
+        print(f"episode score: {total_return}")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--task', type=str, default='pong') 
+    parser.add_argument('--render', action='store_true')
+    parser.add_argument('--cpu', action='store_true')
+    parser.add_argument('--evaluate', type=str, default=None)
+    parser.add_argument('--resume', type=str, default=None, nargs=2)
+    args = parser.parse_args()
+    params = HPS[args.task]
+
+    device = torch.device('cpu') if args.cpu else torch.device('cuda')
+    
+    env = make_env(params.env_name)
+    obs_shape = env.observation_space.shape
+    nb_actions = env.action_space.n
+
+    if params.net_type == 'conv':
+        net = DQN((params.frame_stack, *obs_shape), nb_actions)
+    elif params.net_type == 'linear':
+        raise NotImplementedError
+    agent = DQNAgent(
+        net=net, 
+        nb_actions=nb_actions, 
+        gamma=params.gamma,
+        unroll_steps=params.unroll_steps,
+        device=device
+    )
+
+    if args.evaluate:
+        agent.net.load_state_dict(torch.load(args.evaluate))
+        evaluate(agent, env, render=args.render)
+        exit()
+
+    if args.resume:
+        agent.load(args.resume[0])
+
+    memory = ExperienceBuffer(params.memory_capacity, obs_shape, params.frame_stack,
+                              prioritized=True,
+                              unroll_steps=params.unroll_steps,
+                              beta_start=params.beta_start, beta_end=params.beta_end, beta_steps=params.beta_frames)
+
+    opt = torch.optim.Adam(net.parameters(), lr=params.learning_rate)
+
+    save_id = str(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+    path_str = lambda p: p.absolute().as_posix() # converts a pathlib Path to string
+
+    # Create the run directories
+    runs_dir = Path(f"runs")
+    root_dir = runs_dir / f'{args.task}-{save_id}'
+    chk_dir = root_dir / 'checkpoints'
+
+    runs_dir.mkdir(exist_ok=True) 
+    root_dir.mkdir(exist_ok=True)
+    chk_dir.mkdir(exist_ok=True)
+
+    episode_reward = 0.0
+    episode_count = 0
+
+    obs = env.reset()
+    pb = tqdm(range(-params.memory_initial, params.max_steps))
+    plotter = VisdomLinePlotter()
+    reward_history = deque(maxlen=100)
+    for i in pb:
+        if args.render:
+            env.render()
+
+        # obs = obs.squeeze(0) # TODO: Find a way to eliminate these squeeze calls, not very clean
+        idx = memory.store_obs(obs)
+        state = memory.get_stacked_obs(idx)
+
+        action = agent.act_greedy(state)
+        next_obs, reward, done, _ = env.step(action)
+        episode_reward += reward
+        memory.store_effect(idx, action, np.sign(reward), done)
+
+        if done:
+            next_obs = env.reset()
+            episode_count += 1
+            reward_history.append(episode_reward)
+
+            pb.set_description(f"episode: {episode_count}, reward: {episode_reward}")
+            plotter.plot('episode reward', 'episode return', "Episode Return", episode_count, episode_reward)
+            plotter.plot('episode reward', 'average return', "Episode Return", episode_count, sum(reward_history) / len(reward_history))
+            episode_reward = 0
+            if episode_count > 0 and episode_count % params.save_frequency == 0:
+                agent.save(chk_dir/ f"checkpoint-episode-{episode_count}.pt")
+
+        obs = next_obs
+
+        if i < 0: continue
+
+        if i % params.target_sync == 0:
+            agent.sync_target()
+
+        if i % params.train_frequency == 0:
+            opt.zero_grad()
+            *batch, idx, weights = memory.sample(params.batch_size)
+            loss = agent.calculate_loss(*batch, double=True)
+            loss = loss * weights.to(device)
+            loss.mean().backward()
+            opt.step()
+            memory.update_priorities(idx, loss)
+
+        memory.update_beta(i)
+        pb.update(1)
---
+++ dqn/models/rainbow.py
@@ -0,0 +1,48 @@
+import torch
+import torch.nn as nn
+
+from .helper import get_conv_out
+from .noisy import NoisyLinear
+
+from typing import Tuple
+
+class DQN(nn.Module):
+    def __init__(self, in_shape: Tuple[int, int, int], nb_actions: int):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU(),
+        )
+        self.conv_size = get_conv_out(self.conv, in_shape)
+
+        self.v_head = nn.Sequential(
+            NoisyLinear(self.conv_size, 256),
+            nn.ReLU(),
+            NoisyLinear(256, 1),
+        )
+
+        self.adv_head = nn.Sequential(
+            NoisyLinear(self.conv_size, 256),
+            nn.ReLU(),
+            NoisyLinear(256, nb_actions)
+        )
+
+    def forward(self, x):
+        z = self.conv(x).view(x.shape[0], -1)
+        v = self.v_head(z)
+        adv = self.adv_head(z)
+        return v + (adv - adv.mean(dim=-1, keepdim=True))
+
+
+if __name__ == '__main__':
+    net = DQN((4, 84, 84), 4)
+    print(net)
+
+    x = torch.randn(1, 4, 84, 84)
+    y = net(x)
+
+    print(f"{x.shape} -> {y.shape}")
