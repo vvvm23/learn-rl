19f6cd6596dd632774e7207d1b401846926f615d
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2021-04-13 10:15:38 +0100

    init of categorical DQN forward methods still need to be implemented, maybe look at my prev implementation for engineering ideas?

---
+++ dqn/models/dqn-categorical.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+
+from .helper import get_conv_out
+
+from typing import Tuple
+
+class CategoricalDQN(nn.Module):
+    def __init__(self, in_shape: Tuple[int, int, int], nb_actions: int, 
+            nb_atoms: int = 51, vmin: float = -10., vmax: float = 10.):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU(),
+        )
+
+        self.q_head = nn.Sequential(
+            nn.Linear(get_conv_out(self.conv, in_shape), 512),
+            nn.ReLU(),
+            nn.Linear(512, nb_actions * nb_atoms),
+        )
+
+        dz = (vmax - vmin) / (nb_atoms - 1)
+        self.register_buffer('supports', torch.arange(vmin, vmax+dz, dz))
+
+    def forward(self, x):
+        return self.q_head(self.conv(x).view(x.shape[0], -1))
+
+if __name__ == '__main__':
+    net = DQN((4, 84, 84), 4)
+    print(net)
+
+    x = torch.randn(1, 4, 84, 84)
+    y = net(x)
+
+    print(f"{x.shape} -> {y.shape}")
